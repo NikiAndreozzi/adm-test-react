6a9efca48ba2d0dd3bb9143324c4786508475dd8
Niki Andreozzi <nikiandreozzi@hotmail.it>
2023-08-09 18:03:46 +0200

    feat(tailwind): installed and configured daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+            height: {
+                screen: ['100vh', '100dvh'],
+            },
+            width: {
+                screen: ['100vw', '100dvw'],
+            },
+            container: {
+                padding: {
+                    DEFAULT: '2rem',
+                },
+            },
+        },
     },
-    plugins: [],
+    plugins: [require('daisyui')],
+    daisyui: {
+        themes: ['light'],
+    },
 };
