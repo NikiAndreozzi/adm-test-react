78c50a748a4d2cb84545b0287a915f4168cf0954
Niki Andreozzi <nikiandreozzi@hotmail.it>
2023-08-11 15:12:26 +0200

    feat(planet): created page and route planet

---
+++ src/features/planet/PlanetContainer.jsx
@@ -0,0 +1,33 @@
+import { useEffect } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import { useLazyGetPlanetsQuery } from '../../services/planets';
+
+import Stat from '../../components/Stat';
+import Loader from '../../components/Loader';
+import Error from '../../components/Error';
+
+const PlanetContainer = () => {
+    const { id } = useParams();
+    const navigate = useNavigate();
+
+    const [getPlanet, { data: planet, isLoading, isError }] = useLazyGetPlanetsQuery();
+
+    useEffect(() => {
+        if (id) getPlanet(+id);
+    }, [id]);
+
+    const handleRefetch = () => {
+        getPlanet(+id);
+    };
+
+    return (
+        <section className="h-container w-full mx-auto container">
+            <button className='btn btn-link' onClick={() => navigate(-1)}>&#8592; Go back</button>
+
+            {planet && !isLoading && <Stat planet={planet} />}
+            {isLoading && <Loader />}
+            {isError && !isLoading && <Error refetch={handleRefetch} />}
+        </section>
+    );
+};
+export default PlanetContainer;
--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,5 +1,7 @@
-import App from '../App';
 import { createBrowserRouter, Navigate } from 'react-router-dom';
+
+import App from '../App';
+import PlanetContainer from '../features/planet/PlanetContainer';
 
 const router = createBrowserRouter([
     {
@@ -8,7 +10,7 @@ const router = createBrowserRouter([
     },
     {
         path: '/planets/:id',
-        element: <div>planets</div>,
+        element: <PlanetContainer />,
     },
     {
         path: '*',
